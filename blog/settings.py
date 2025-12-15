e872db4c5643644f0101541a69f827b8911774ab
darakhshanda <darakhshanda@hotmail.com>
2025-12-15 23:07:24 +0000

    add env.py

--- blog/settings.py
+++ blog/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lin2b4l1v7(lalsz3r5j)o2y*9u@=xw-v*_+fl!4gq)nnqcppk'
+if os.path.isfile("env.py"):
+    import env
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
