8ca42f214286da35a012c0d14b648267a649251c
darakhshanda <darakhshanda@hotmail.com>
2025-12-24 00:13:59 +0000

    Add: commit model, make migrations

--- mainblog/models.py
+++ mainblog/models.py
@@ -29,10 +29,12 @@ class User(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(
         Post, on_delete=models.CASCADE, related_name="comments")
-    author =models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
+
 
 def __str__(self):
     return f"Post for {self.title}"
