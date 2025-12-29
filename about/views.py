0711304c36ea54d94943e6e7e83a0482f809a223
darakhshanda <darakhshanda@hotmail.com>
2025-12-29 06:16:57 +0000

    added comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,15 +1,27 @@
 from django.shortcuts import render
 from .models import About
+from .forms import CollaborateForm
+from django.contrib import messages
 
 
 def about_me(request):
-    """
-    Renders the About page
-    """
     about = About.objects.all().order_by('-updated_on').first()
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                "Collaboration request received! I endeavor to respond within 2 working days."
+            )
+
+    collaborate_form = CollaborateForm()
 
     return render(
         request,
         "about/about.html",
-        {"about": about},
+        {
+            "about": about,
+            "collaborate_form": collaborate_form},
     )
